d2ca66155f26d8e6f4b770d0d0750846b4c33ccb
Nematjon100 <nematdev@gmail.com>
2023-12-10 17:00:24 +0500

    Add country-page

--- js/main.js
+++ js/main.js
@@ -1,5 +1,11 @@
 const elSiteThemeToggler = document.querySelector(".site-header-theme-toggler");
+const elButton = document.querySelector('.button');
+
 
 elSiteThemeToggler.addEventListener('click', function () {
   document.body.classList.toggle('dark-mode');
+})
+
+elButton.addEventListener('click', function () {
+
 })
